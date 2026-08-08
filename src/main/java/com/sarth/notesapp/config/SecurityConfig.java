1535caa1742f6c175d832256f874ac66ee56573f
Sarth Mishra <mishrasarth2186@gmail.com>
2026-08-08 15:02:36 +0530

    Add method level security

--- src/main/java/com/sarth/notesapp/config/SecurityConfig.java
+++ src/main/java/com/sarth/notesapp/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.sarth.notesapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
@@ -22,7 +24,6 @@ public class SecurityConfig {
         http
                         .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/register").permitAll()
-                                .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .httpBasic(basic -> {})
--- src/main/java/com/sarth/notesapp/controller/AdminController.java
+++ src/main/java/com/sarth/notesapp/controller/AdminController.java
@@ -1,11 +1,13 @@
 package com.sarth.notesapp.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AdminController {
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin/test")
     public String adminTest(){
         return "Admin access granted";
