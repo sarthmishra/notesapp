d4d7d458533a7313b113bba8054eb8cee477e3df
Sarth Mishra <mishrasarth2186@gmail.com>
2026-08-14 17:26:32 +0530

    Add JWT authentication filter to validate Bearer tokens

--- src/main/java/com/sarth/notesapp/config/SecurityConfig.java
+++ src/main/java/com/sarth/notesapp/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.sarth.notesapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -8,11 +9,17 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -28,7 +35,8 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .httpBasic(basic -> {})
-                .csrf(csrf -> csrf.disable());
+                .csrf(csrf -> csrf.disable()
+                        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class));
 
         return http.build();
     }
--- src/main/java/com/sarth/notesapp/service/JwtService.java
+++ src/main/java/com/sarth/notesapp/service/JwtService.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Claims;
+
 @Service
 public class JwtService {
 
@@ -27,4 +30,28 @@ public class JwtService {
                 .signWith(getSigningKey())
                 .compact();
     }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            extractAllClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Claims extractAllClaims(String token) {
+        Jws<Claims> jws = Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token);
+
+        return jws.getPayload();
+    }
+
+
 }
